a4375fb43e6fc106fe1e9cd9b4ad6575dffe14ed
Jaquelinatalie <106705662+Jaquelinatalie@users.noreply.github.com>
2023-05-13 12:16:14 -0600

    Delete Ejercicio3.py

--- Ejercicio3.py
+++
@@ -1,9 +0,0 @@
-#
-#Entrada de datos
-año_nacimiento = int(input('Ingrese su año de nacimiento: '))
-
-#Proceso
-operación = 2023 - año_nacimiento
-
-#Salida de datos
-print("Tu edaad es: ", operación, "años")
