40b11ea3743b1f0095709b6c13b9044e486fd04d
Jaquelinatalie <106705662+Jaquelinatalie@users.noreply.github.com>
2023-05-13 09:51:19 -0600

    Tipos de datos

---
+++ Curso IPN/TiposDeDatos.py
@@ -0,0 +1,25 @@
+# Tipos de datos
+
+#Declaración de variables
+
+import datetime
+
+
+nombre = "Jaqueline"
+edad = 29
+fecha_nacimiento = datetime.date(1994, 4, 22)
+estatura = 1.65
+direccion = "Valle del Siret 116"
+teléfono = "5537044766"
+estatus = True
+colores_favoritos = ["negro", "verde", "rojo"]
+
+#Salida de datos
+print("Nombre: ", type(nombre))
+print("Edad: ", type(edad))
+print("Fecha de nacimiento: ", type(fecha_nacimiento))
+print("Estatura: ", type(estatura))
+print("Dirección: ", type(direccion))
+print("Télefono: ", type(teléfono))
+print("Estatus: ", type(estatus))
+print("Colores favoritos: ", type(colores_favoritos))
